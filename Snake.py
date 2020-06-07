d3b42d178dcab6ec6a5e168f5644d0527a8112d5
Ju4nCa <ballesteros.juan@correounivalle.edu.co>
2020-06-07 13:11:09 -0500

    Cola de la serpiente funcional

--- Snake.py
+++ Snake.py
@@ -21,28 +21,23 @@ dimX= size[0] // nxC
 dimY= size[1] // nyC
 
 matriz= np.zeros((nxC , nyC))
-snake=2
-cola=0
+snake = 2
 serx, sery = 5, 10
-colx, coly = 5-snake, 10
 eatx, eaty = random.randint(0,nyC-1), random.randint(0,nyC-1)
 matriz[serx, sery]=1
-matriz[eatx, eaty]=2
-key = 'righ'
-key2= 'rigt'
+colax, colay = 5, 10
+key = 'right'
+keyCola = 'right'
+direc = []
+game_over = False
+
+i=1 #Retraso para la cola
 
 pygame.display.flip()
 
-def game_over():
-     final = False
-     if (serx < 0 or sery < 0):
-          final = True
-     if(serx > nxC-1 or sery > nyC-1):
-          final = True
-     return final
 #BUCLE DE EJECUCION
-while not game_over():
-
+while not game_over:
+     
      newMatriz = np.copy(matriz)
      screen.fill(bg)
      time.sleep(0.1)
@@ -50,52 +45,71 @@ while not game_over():
      newMatriz[eatx, eaty]=2
      
      ev = pygame.event.get()
-     newMatriz[colx,coly]=2
-     newMatriz[serx,sery]=1
-     print(cola)
+
+     if serx >= nxC:
+          serx = 0
+     if colax >= nxC:
+          colax = 0
+     if sery >= nyC:
+          sery = 0
+     if colay >= nyC:
+          colay = 0
+          
+     if i > snake:
+          keyCola = direc[0]
+          if (keyCola == 'right'):
+               newMatriz[colax, colay]=0
+               colax+=1
+
+          if (keyCola == 'left'):
+               newMatriz[colax, colay]=0
+               colax-=1
+                    
+          if (keyCola == 'up'):
+               newMatriz[colax, colay]=0
+               colay-=1
+               
+          if (keyCola == 'down'):
+               newMatriz[colax, colay]=0
+               colay+=1
+
+          direc.pop(0)
+     else:
+          i+=1
+     
      #MOVIMIENTO DE LA SERPIENTE
      if (key == 'right'):
+          newMatriz[serx,sery]=1
           serx+=1
-          if(coly==sery):
-               key2='right'
-                
+
      if (key == 'left'):
+          newMatriz[serx,sery]=1
           serx-=1
-          if(coly==sery):
-               key2='left'
                
      if (key == 'up'):
+          newMatriz[serx,sery]=1
           sery-=1
-          if(colx==serx):
-               key2='up'
-       
           
      if (key == 'down'):
+          newMatriz[serx,sery]=1
           sery+=1
-          if(colx==serx):
-               key2='down'
 
-     if(cola>=snake):
-          newMatriz[colx,coly]=2
-          if (key2 == 'right'):
-               colx+=1
-                     
-          if (key2 == 'left'):
-               colx-=1
-                    
-          if (key2 == 'up'):
-               coly-=1
-               
-          if (key2 == 'down'):
-               coly+=1
-     else:
-          cola+=1
+     direc.append(key)
+
+     if serx < 0:
+          serx = nxC-1
+     if colax < 0:
+          colax = nxC-1
+     if sery < 0:
+          sery = nyC-1
+     if colay < 0:
+          colay = nyC-1
      
      if((serx, sery)==(eatx, eaty)):
          newMatriz[eatx,eaty]=0
          eatx, eaty = random.randint(0,nyC-1), random.randint(0,nyC-1)
          snake+=1
-         cola=0
+         print(snake)
      
      for event in ev:
           if event.type is pygame.QUIT:
@@ -113,7 +127,7 @@ while not game_over():
                        ((x+1) * dimX,   (y+1) * dimY),
                        ((x)   * dimX,   (y+1) * dimY)]
                if (newMatriz[x, y]==0):
-                    pygame.draw.polygon(screen, (125,125,125), poly, 0)
+                    pygame.draw.polygon(screen, (125,125,125), poly, 1)
                elif(newMatriz[x, y]==1):
                     pygame.draw.polygon(screen, (255,255,255), poly, 0)
                else:
