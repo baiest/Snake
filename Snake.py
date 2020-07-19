4746f1e3ad0a2fee4aaf9dda15adef59ebf7a9d1
Ju4nCa <ballesteros.juan@correounivalle.edu.co>
2020-07-19 13:12:22 -0500

    condiciones agregadas

--- Snake.py
+++ Snake.py
@@ -4,15 +4,14 @@ import numpy as np
 import os
 import time
 import pygame
+from pygame.locals import *
 import random
 
 pygame.init()
-
-size= (500, 500)
+size= (600, 600)
 screen = pygame.display.set_mode(size)
 
-bg= 25,25, 25
-
+bg = (0,0,0)
 screen.fill(bg)
 
 nxC, nyC = 50, 50
@@ -31,6 +30,9 @@ keyCola = 'right'
 direc = []
 game_over = False
 running = True
+flag = True
+vel = 0.03
+boost = 5
 
 i=1 #Retraso para la cola
 
@@ -38,34 +40,26 @@ pygame.display.flip()
 
 #BUCLE DE EJECUCION
 while not game_over:
-     
+     if snake == boost:
+          vel= vel / 10
+          boost += boost
+
      newMatriz = np.copy(matriz)
      screen.fill(bg)
-     time.sleep(0.03)
+     time.sleep(vel)
 
      newMatriz[eatx, eaty]=2
      
      ev = pygame.event.get()
-
-     if serx >= nxC:
-          serx = 0
-     if colax >= nxC:
-          colax = 0
-     if sery >= nyC:
-          sery = 0
-     if colay >= nyC:
-          colay = 0
      
-     if key not in ['right', 'left', 'up', 'down']:
-          running = False
      if running:     
           if i > snake:
                keyCola = direc[0]
                if (keyCola == 'right'):
                     newMatriz[colax, colay]=0
                     colax+=1
-
-               if (keyCola == 'left'):
+               
+               if ( keyCola == 'left'):
                     newMatriz[colax, colay]=0
                     colax-=1
                          
@@ -98,11 +92,20 @@ while not game_over:
                newMatriz[serx,sery]=1
                sery+=1
           try:
-               if  newMatriz[serx,sery]==1:
+               if newMatriz[serx,sery]==1:
                     game_over = True
           except IndexError:
                pass
           direc.append(key)
+     
+     if serx > nxC-1:
+          serx = 0
+     if colax > nxC-1:
+          colax = 0
+     if sery > nyC-1:
+          sery = 0
+     if colay > nyC-1:
+          colay = 0
 
      if serx < 0:
           serx = nxC-1
@@ -114,10 +117,13 @@ while not game_over:
           colay = nyC-1
      
      if((serx, sery)==(eatx, eaty)):
-         newMatriz[eatx,eaty]=0
-         eatx, eaty = random.randint(0,nyC-1), random.randint(0,nyC-1)
-         snake+=1
-         print(snake)
+          newMatriz[eatx,eaty]=0
+          while True:
+              eatx, eaty = random.randint(0,nyC-1), random.randint(0,nyC-1)
+              if (matriz[eatx,eaty]!=1):
+                   break
+          snake+=1 
+          print(snake)
      
      for event in ev:
           if event.type is pygame.QUIT:
@@ -125,8 +131,22 @@ while not game_over:
                 break
      
           if (event.type is pygame.KEYDOWN):
-               key = pygame.key.name(event.key)
-                    
+               newKey = pygame.key.name(event.key)
+               if newKey == 'right' and key !='left':
+                    key = newKey
+               if newKey == 'up' and key !='down':
+                    key = newKey
+               if newKey == 'left' and key !='right':
+                    key = newKey
+               if newKey == 'down' and key !='up':
+                    key = newKey
+
+               if newKey not in ['right', 'left', 'up', 'down']:
+                    running = False
+               else:
+                    running = True
+               print(newKey)
+
      for y in range(0, nyC):
           for x in range (0, nxC):
                               
@@ -134,11 +154,9 @@ while not game_over:
                        ((x+1) * dimX,   y     * dimY),
                        ((x+1) * dimX,   (y+1) * dimY),
                        ((x)   * dimX,   (y+1) * dimY)]
-               if (newMatriz[x, y]==0):
-                    pygame.draw.polygon(screen, (0, 0, 0), poly, 0)
-               elif(newMatriz[x, y]==1):
+               if(newMatriz[x, y]==1):
                     pygame.draw.polygon(screen, (255,255,255), poly, 0)
-               else:
+               if(newMatriz[x, y]==2):
                     pygame.draw.polygon(screen, (0,255,0), poly, 0)
 
      pygame.display.flip()
